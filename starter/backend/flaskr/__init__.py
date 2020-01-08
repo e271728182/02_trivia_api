99e4cfff9dedce0bd85d684cdc4b1f748e30ed9c
Gabriel <gab.patterson@gmail.com>
2020-01-08 11:00:27 -0500

    code cleanup

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -30,8 +30,10 @@ def create_app(test_config=None):
 
   @app.after_request
   def after_request(response):
-      response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization,true")
-      response.headers.add("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,OPTIONS")
+      response.headers.add("Access-Control-Allow-Headers",
+      "Content-Type,Authorization,true")
+      response.headers.add("Access-Control-Allow-Methods",
+      "GET,PATCH,POST,DELETE,OPTIONS")
       return response
 
   @app.route('/categories',methods=['GET'])
@@ -110,7 +112,8 @@ def create_app(test_config=None):
       input=request.get_json()
       search_term=input.get('search_term')
 
-      search_results = Question.query.filter(Question.question.ilike("%{}%".format(search_term))).all()
+      search_results = (Question.query.filter(Question.question.ilike("%{}%"
+      .format(search_term))).all())
       if len(search_results)==0:
           abort(404)
 
@@ -146,8 +149,9 @@ def create_app(test_config=None):
       #filter questions over the selected category and eliminate
       #questions already played in previous rounds which are stored if __name__ == '__main__':
       # previous_questions list
-      questions=Question.query.filter_by(category=quizCategoryId).filter(Question.id.notin_(previousQuestions)).all()
-    
+      questions=(Question.query.filter_by(category=quizCategoryId)
+      .filter(Question.id.notin_(previousQuestions)).all())
+
       #select as random question among the filtered ones
       question=random.choice(questions).format()
 
@@ -177,46 +181,5 @@ def create_app(test_config=None):
       'error':500,
       'message':'there was an error on the server side'}),500)
 
-  '''
- @TODO:
- XCreate a GET endpoint to get questions based on category.
-
- TEST: In the "List" tab / main screen, clicking on one of the
- categories in the left column will cause only questions of that
- category to be shown.
-    @TODO:
-    XCreate an endpoint to POST a new question,
-    which will require the question and answer text,
-    category, and difficulty score.
-
-    TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.
-
-    @TODO:
-    XCreate a POST endpoint to get questions based on a search term.
-    It should return any questions for whom the search term
-    is a substring of the question.
-
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
-
-  @TODO:
-  Create a POST endpoint to get questions to play the quiz.
-  This endpoint should take category and previous question parameters
-  and return a random questions within the given category,
-  if provided, and that is not one of the previous questions.
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not.
-  '''
-
-  '''
-  @TODO:
-  Create error handlers for all expected errors
-  including 404 and 422.
-  '''
 
   return app
